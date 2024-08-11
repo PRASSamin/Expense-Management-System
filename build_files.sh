e519f4016e8862efdd298165b622ba99f80fd1f8
PRASSamin <prassamin@gmail.com>
2024-08-11 07:39:53 +0600

    v0.0.2

--- build_files.sh
+++ build_files.sh
@@ -1,21 +1,19 @@
 echo "BUILD START"
 
-apt-get update
-apt-get install -y libmysqlclient-dev
-
 if ! command -v pip3 &> /dev/null; then
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3 get-pip.py --user
     export PATH=$PATH:~/.local/bin
 fi
 
-python3 -m venv venv
+python3.9 -m venv venv
 source venv/bin/activate
 
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
-python manage.py collectstatic --noinput --clear
-python manage.py makemigrations
-python manage.py migrate
+python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+
 
 echo "BUILD END"
